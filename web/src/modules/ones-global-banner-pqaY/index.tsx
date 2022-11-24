0adac7692e12d1589641a547a06c4d42f40a3b1f
caoxing-mike <caoxing@ones.cn>
2022-11-24 19:09:09 +0800

    update: config

---
+++ web/src/modules/ones-global-banner-pqaY/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ConfigProvider } from '@ones-design/core'
+import './index.css'
+
+ReactDOM.render(<ConfigProvider>ttt</ConfigProvider>, document.getElementById('ones-mf-root'))
